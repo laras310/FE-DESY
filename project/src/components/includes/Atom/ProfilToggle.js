d369cbac35810b2bc1e0ab04d454717155cfcf88
laras310 <nurullarasati310@gmail.com>
2023-11-17 09:52:32 +0700

    ganti avatar rsuite

--- project/src/components/includes/Atom/ProfilToggle.js
+++ project/src/components/includes/Atom/ProfilToggle.js
@@ -1,14 +1,21 @@
-import { Image } from "react-bootstrap";
+
+import { Avatar } from "rsuite";
 export default function ProfilToggle({ onClick }) {
     return (
-      <Image
-        src="/assets/images/profil.jpg"
-        roundedCircle
-        height="40"
-        className="d-inline-block align-top"
-        alt="Toggle Dropdown"
-        onClick={onClick}
-        style={{ cursor: 'pointer' }}
+      <Avatar
+      src="/assets/images/profil.jpg"
+      onClick={onClick}
+      style={{ cursor: 'pointer' }}
+      circle
       />
+      // <Image
+      //   src="/assets/images/profil.jpg"
+      //   roundedCircle
+      //   height="40"
+      //   className="d-inline-block align-top"
+      //   alt="Toggle Dropdown"
+      //   onClick={onClick}
+      //   style={{ cursor: 'pointer' }}
+      // />
     );
   }
--- project/src/components/includes/TimelineOnly.js
+++ project/src/components/includes/TimelineOnly.js
@@ -5,61 +5,32 @@ import axios from 'axios';
 import { FileArrowDown } from 'react-bootstrap-icons';
 
 export default function TimelineOnly({data}){
-    const [activity, setActivity] = useState([]);
+    // function axiosDownloadFile(url, fileName) {
+    //   return axios({
+    //     url,
+    //     headers:{         'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Headers': '*',},
+    //     method: 'GET',
+    //     responseType: 'blob',
+    //   })
+    //     .then(response => {
+    //       const href = window.URL.createObjectURL(response.data);
     
-    useEffect(() => {
-      setActivity(data.activities)
-    //     axios({
-    //         method: "GET",
-    //         url: "https://jobcard-api.pins.co.id/api/task?task_id="+task_id,
-    //         // url: "https://jobcard-api.pins.co.id/api/task/by-user?user_id=" + profil.id,
-    //         headers: {
-    //           Authorization: 'Bearer ' + localStorage.getItem('access_token')
-    //         },
-    //       })
-    //     .then((response)=>{
-    //         const res= response.data.data
-    //         setActivity(res.activities)
+    //       const anchorElement = document.createElement('a');
+    
+    //       anchorElement.href = href;
+    //       anchorElement.download = fileName;
+    
+    //       document.body.appendChild(anchorElement);
+    //       anchorElement.click();
+    
+    //       document.body.removeChild(anchorElement);
+    //       window.URL.revokeObjectURL(href);
     //     })
-    //     .catch((error) => {
-    //         if (error.response) {
-    //           console.log(error.response.data);
-    //           console.log(error.response.status);
-    //           console.log(error.response.headers);
-    //         } else if (error.request) {
-    //           console.log(error.request);
-    //         } else {
-    //           console.log(error.message);
-    //         }
-    //       });
-    },[])
-
-    function axiosDownloadFile(url, fileName) {
-      return axios({
-        url,
-        headers:{         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',},
-        method: 'GET',
-        responseType: 'blob',
-      })
-        .then(response => {
-          const href = window.URL.createObjectURL(response.data);
-    
-          const anchorElement = document.createElement('a');
-    
-          anchorElement.href = href;
-          anchorElement.download = fileName;
-    
-          document.body.appendChild(anchorElement);
-          anchorElement.click();
-    
-          document.body.removeChild(anchorElement);
-          window.URL.revokeObjectURL(href);
-        })
-        .catch(error => {
-          console.log('error: ', error);
-        });
-    }
+    //     .catch(error => {
+    //       console.log('error: ', error);
+    //     });
+    // }
     return(
         <Timeline align="left">
             <Timeline.Item>
